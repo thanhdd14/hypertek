d8fc5aff6f86e77564b6101cf5bc075b29531124
thanhdd <duythanh14189@gmail.com>
2024-08-20 20:33:11 +0700

    up

---
+++ js/setting.js
@@ -0,0 +1,72 @@
+//menu header
+$('.js-mobile').on('click', function(){
+    $(this).toggleClass("js-mobile--close");
+    $("html").toggleClass("js-locked");
+    // $(".nav-menu").slideToggle();
+    // e.preventDefault();
+    $(".header-nav").fadeToggle();
+});
+
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 10) {
+        $("#header").addClass("js-fixed");
+    }
+    else {
+        $("#header").removeClass("js-fixed");
+    }
+});
+
+$(function () {
+    var headerHeight = $('#header').outerHeight();
+    var urlHash = location.hash;
+    if (urlHash) {
+        $('body,html').stop().scrollTop(0);
+        setTimeout(function () {
+            var target = $(urlHash);
+            var position = target.offset().top - headerHeight;
+            $('body,html').stop().animate({scrollTop: position}, 1000);
+        }, 100);
+    }
+    
+    jQuery('.js-anchor').click(function(e) {
+        jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - headerHeight}, 700);
+        return false;
+        e.preventDefault();
+    });
+    jQuery('.js-header-anchor').click(function(e) {
+        $(".js-mobile").removeClass("js-mobile--close");
+        $("html").removeClass("js-locked");
+        $(".header-nav").fadeOut();
+        jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - headerHeight}, 700);
+        return false;
+        e.preventDefault();
+    });
+});
+
+
+
+//fade
+$(window).on('scroll load assessFeatureHeaders', function(){
+    var scrollTop = $(window).scrollTop();
+    var appearenceBuffer = 60;
+    var windowBottom = scrollTop + $(window).height() - appearenceBuffer;
+    $('body').toggleClass('scrolled-down', scrollTop > 0);
+    $('.js-scrollin:not(.active)').filter(function(){
+        var offset = $(this).offset().top;
+        var height = $(this).outerHeight();
+        return offset + height >= scrollTop && offset <= windowBottom;
+    }).addClass('active');
+});
+
+
+//backtop
+jQuery(document).ready(function ($) {
+    $('#backtop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
